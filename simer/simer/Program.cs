298d31abb44dd40d7bb701288ec767ef8330133e
Simerpreet Singh <726010@student.tor.loc>
2019-03-28 17:12:54 -0400

    final commit

---
+++ simer/simer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            Download();
+            Console.ReadLine();
+        }
+        static async void Download()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("http://ibm.com");
+
+            Console.WriteLine(data);
+            List myList = new List();
+        }
+    }
+    class Network
+    {
+        static public Task Download()
+        {
+
+            return Task.Run(() => Thread.Sleep(3000));
+        }
+    }
+}
